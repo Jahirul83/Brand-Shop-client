2d19dd136f5113ad3102ee33b6b0750ac5ed4f7b
Jahirul83 <jahirulislamcse83.com>
2023-10-19 16:30:25 +0600

    router tocomponents like login sign up and Home page banner created

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Header/Navbar";
+import Footer from "../components/Footer/Footer";
 
 
 const Root = () => {
     return (
-        <div>
+        <div className="max-w-6xl mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
