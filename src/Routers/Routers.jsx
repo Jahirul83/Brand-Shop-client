d2b89300669453f9f098a031016687b03358037e
Jahirul83 <jahirulislamcse83@gmail.com>
2023-10-20 18:34:09 +0600

    product update or edit and show product detail by id

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -5,6 +5,8 @@ import SignUp from "../components/signUp/signUp";
 import Home from "../components/Home/Home";
 import AddProduct from "../components/AddProduct/AddProduct";
 import Products from "../components/Products/Products";
+import UpdateProduct from "../components/UpdateProduct/UpdateProduct";
+import ProductDetails from "../components/Products/productDetails";
 
 
 const router = createBrowserRouter([
@@ -33,6 +35,17 @@ const router = createBrowserRouter([
       {
         path: '/signup',
         element: <SignUp></SignUp>
+      },
+      {
+        path: '/updateproduct/:id',
+        element: <UpdateProduct></UpdateProduct>,
+        loader: ({ params }) => fetch(`http://localhost:5000/products/${params.id}`)
+      },
+      {
+        path: '/productDetails/:id',
+        element: <ProductDetails></ProductDetails>,
+        loader: ({ params }) => fetch(`http://localhost:5000/products/${params.id}`)
+
       }
     ]
   },
