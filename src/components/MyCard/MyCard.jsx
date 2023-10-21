31e47123e66cd2ecb9a20ab6c98e9185999e9e48
Jahirul83 <jahirulislamcse83@gmail.com>
2023-10-21 21:21:24 +0600

    small edit in home and signIn signUp

--- src/components/MyCard/MyCard.jsx
+++ src/components/MyCard/MyCard.jsx
@@ -6,7 +6,7 @@ const MyCard = () => {
     const cartData = useLoaderData();
     return (
         <div>
-            <h2>this is my cart:{cartData.length}</h2>
+            {/* <h2>this is my cart:{cartData.length}</h2> */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
                 {
                     cartData.map(cart => <Cart
