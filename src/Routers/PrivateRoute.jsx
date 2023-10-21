c2e8268b53d06ef9b2221523444d493670316cca
Jahirul83 <jahirulislamcse83.com>
2023-10-21 16:55:23 +0600

    navigate state after login

--- src/Routers/PrivateRoute.jsx
+++ src/Routers/PrivateRoute.jsx
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useContext(AuthContext)
+
+
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
+
+    console.log(location);
 
     if (loading) {
         return <span className="loading loading-dots loading-lg"></span>;
@@ -11,7 +16,7 @@ const PrivateRoute = ({ children }) => {
     if (user) {
         return children;
     }
-    return <Navigate to='/signIn'></Navigate>
+    return <Navigate state={location.pathname} to='/signIn'></Navigate>
 };
 
 export default PrivateRoute;
